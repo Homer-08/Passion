511bdb2807203819739cefc7639586af6a5fbdaa
Vlad Serbulov <mellnote71@gmail.com>
2021-07-03 11:47:58 +0900

    Add user profile file and main background image

---
+++ js/user_profile.js
@@ -0,0 +1,18 @@
+export let user_profile_stats = {
+    monsters_killed: 0,
+    dead_count: 0,
+    items_received: 0
+}
+
+export let user_stats = {
+    attack: 10,                 //user attack rating
+    heal_point: 100,            //user heal points
+    crit: 0,                    //crit power
+    crit_chance: 0,             //crit chance
+    level: 1,                   //user level
+    gold: 0,                    //type of deffault money
+    axe: 0                      //types of rare money
+}
+
+export let user_dressed_items = [{}]
+export let user_inventory_items = [{}]
