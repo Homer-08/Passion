4bcb9d6642128984910043fdcdfcda1db101a686
nikolas <nikolaypunko777@gmail.com>
2021-07-06 17:25:46 +0300

    Output hero stats on the main page

--- js/user_profile.js
+++ js/user_profile.js
@@ -5,14 +5,32 @@ export let user_profile_stats = {
 }
 
 export let user_stats = {
-    attack: 10,                 //user attack rating
-    heal_point: 100,            //user heal points
-    crit: 0,                    //crit power
-    crit_chance: 0,             //crit chance
     level: 1,                   //user level
     gold: 0,                    //type of deffault money
-    axe: 0                      //types of rare money
+    axe: 0,                      //types of rare money
+    attack: 10,                 //user attack rating
+    heal_point: 0,            //user heal points
+    crit: 0,                    //crit power
+    crit_chance: 0             //crit chance
 }
 
 export let user_dressed_items = [{}]
 export let user_inventory_items = [{}]
+
+let userStats = ""
+
+let HTMLUserStats = document.getElementById("user_stats_HTML")
+
+function showUserStats() {
+    userStats = '<div class="col-12" id="userStats">' + 'Hero Level: ' + user_stats.level + '</div>' +
+        '<div class="col-12" id="userStats">' + 'Gold: ' + user_stats.gold + '</div>' +
+        '<div class="col-12" id="userStats">' + 'Axe: ' + user_stats.axe + '</div>' +
+        '<div class="col-12" id="userStats">' + 'Hero Attack: ' + user_stats.attack + '</div>' +
+        '<div class="col-12" id="userStats">' + 'Hero Health Point: ' + user_stats.heal_point + '</div>' +
+        '<div class="col-12" id="userStats">' + 'Crit Damage: ' + user_stats.crit + '</div>' +
+        '<div class="col-12" id="userStats">' + 'Crit Chance: ' + user_stats.crit_chance + '</div>'
+}
+ 
+showUserStats()
+
+HTMLUserStats.insertAdjacentHTML('afterbegin', userStats)
