b347ff183d351aeff77141e91f09bd7660d5cb0e
nikolas <nikolaypunko777@gmail.com>
2021-07-07 12:01:41 +0300

    Improving the method of output stats

--- js/user_profile.js
+++ js/user_profile.js
@@ -1,3 +1,5 @@
+let HTMLUserStats = document.getElementById("user_stats_HTML")
+
 export let user_profile_stats = {
     monsters_killed: 0,
     dead_count: 0,
@@ -7,30 +9,35 @@ export let user_profile_stats = {
 export let user_stats = {
     level: 1,                   //user level
     gold: 0,                    //type of deffault money
-    axe: 0,                      //types of rare money
+    axe: 0,                     //types of rare money
     attack: 10,                 //user attack rating
-    heal_point: 0,            //user heal points
+    heal_point: 0,              //user heal points
     crit: 0,                    //crit power
-    crit_chance: 0             //crit chance
+    crit_chance: 0              //crit chance
 }
 
 export let user_dressed_items = [{}]
 export let user_inventory_items = [{}]
 
-let userStats = ""
+function splitString(stringToSplit) {
+    var result = stringToSplit.split('_')
 
-let HTMLUserStats = document.getElementById("user_stats_HTML")
+    for (var i = 0; i <= result.length - 1; i++) {
+        result[i] = result[i].charAt(0).toUpperCase() + result[i].slice(1)
+    }
+
+    return result.join(' ')
+}
 
 function showUserStats() {
-    userStats = '<div class="col-12" id="userStats">' + 'Hero Level: ' + user_stats.level + '</div>' +
-        '<div class="col-12" id="userStats">' + 'Gold: ' + user_stats.gold + '</div>' +
-        '<div class="col-12" id="userStats">' + 'Axe: ' + user_stats.axe + '</div>' +
-        '<div class="col-12" id="userStats">' + 'Hero Attack: ' + user_stats.attack + '</div>' +
-        '<div class="col-12" id="userStats">' + 'Hero Health Point: ' + user_stats.heal_point + '</div>' +
-        '<div class="col-12" id="userStats">' + 'Crit Damage: ' + user_stats.crit + '</div>' +
-        '<div class="col-12" id="userStats">' + 'Crit Chance: ' + user_stats.crit_chance + '</div>'
+    let userStats = ""
+
+    for (var key in user_stats) {
+        userStats += '<div class="col-12" id="userStats">' + splitString(key) + ': ' + user_stats[key] + '</div>'
+    }
+    
+    HTMLUserStats.insertAdjacentHTML('afterbegin', userStats)
 }
  
 showUserStats()
 
-HTMLUserStats.insertAdjacentHTML('afterbegin', userStats)
