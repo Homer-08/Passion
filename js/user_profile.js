3f2a60413cd300ea9d90aae1b94e3637af2f1303
Homer-08 <81813135+Homer-08@users.noreply.github.com>
2021-07-07 12:56:35 +0300

    Feature/gi 0004 (#4)
    * Create main page and navbar for him
    
    * Rename file style.css to entrance_style.css
    
    * Change the transaction time for file entrance_style.css
    
    * Add hero image on main page
    
    * Update hero position image
    
    * Update position hero image
    
    * Output hero stats on the main page
    
    * Improving the method of output stats

--- js/user_profile.js
+++ js/user_profile.js
@@ -1,3 +1,5 @@
+let HTMLUserStats = document.getElementById("user_stats_HTML")
+
 export let user_profile_stats = {
     monsters_killed: 0,
     dead_count: 0,
@@ -5,14 +7,37 @@ export let user_profile_stats = {
 }
 
 export let user_stats = {
-    attack: 10,                 //user attack rating
-    heal_point: 100,            //user heal points
-    crit: 0,                    //crit power
-    crit_chance: 0,             //crit chance
     level: 1,                   //user level
     gold: 0,                    //type of deffault money
-    axe: 0                      //types of rare money
+    axe: 0,                     //types of rare money
+    attack: 10,                 //user attack rating
+    heal_point: 0,              //user heal points
+    crit: 0,                    //crit power
+    crit_chance: 0              //crit chance
 }
 
 export let user_dressed_items = [{}]
 export let user_inventory_items = [{}]
+
+function splitString(stringToSplit) {
+    var result = stringToSplit.split('_')
+
+    for (var i = 0; i <= result.length - 1; i++) {
+        result[i] = result[i].charAt(0).toUpperCase() + result[i].slice(1)
+    }
+
+    return result.join(' ')
+}
+
+function showUserStats() {
+    let userStats = ""
+
+    for (var key in user_stats) {
+        userStats += '<div class="col-12" id="userStats">' + splitString(key) + ': ' + user_stats[key] + '</div>'
+    }
+    
+    HTMLUserStats.insertAdjacentHTML('afterbegin', userStats)
+}
+ 
+showUserStats()
+
