00c7ef6db4241d9f36325768b58289747fc0ee46
nikolas <nikolaypunko777@gmail.com>
2021-07-28 16:52:16 +0300

    Dungeon page creation

--- js/dungeons.js
+++ js/dungeons.js
@@ -1,11 +1,14 @@
 let HTMLDungeonList = document.getElementById("dungeon_list_HTML")
+let HTMLModalWindowForFirstDungeon = document.getElementById("modal_window_for_first_dundeon")
+
 
 export let dungeon_list = {
     dungeon_1: {
         id: 1,
         name: "Сastle of giants",
         max_number_mobs: 9,
-        src: "/resources/images/dungeons/castle_of_giants.jpg"
+        src: "/resources/images/dungeons/castle_of_giants.jpg",
+        id_mobs: [1, 2, 3, 4, 5, 6, 7, 8, 9]
     },
     dungeon_2: {
         id: 2,
@@ -43,10 +46,35 @@ function showDungeonList() {
     let resDungeonList = ""
 
     for (var key in dungeon_list) {
-        console.log(dungeon_list[key].src)
-        resDungeonList += '<div class="col-4 mt-3 text-center"><img class="img_shadow" src="' + dungeon_list[key].src + '"><button type="button" class="btn btn-secondary m-3 btn-lg btn_shadow" data-toggle="modal" data-target="#exampleModalLong">' + dungeon_list[key].name + '</button></div>'
+        resDungeonList += '<div class="col-4 mt-3 text-center"><img class="img_shadow" src="' + dungeon_list[key].src +
+            '"><button type="button" class="btn btn-secondary m-3 btn-lg btn_shadow" data-toggle="modal" data-target="#' + key + 'ModalWindow">' + dungeon_list[key].name + '</button></div>'
     }
     HTMLDungeonList.insertAdjacentHTML('afterbegin', resDungeonList)
 }
 
+function showFirstMobsList() {
+    let first_mobs_list = ""
+
+    let mobs_array = dungeon_list.dungeon_1.id_mobs
+    let temp = pull_data('allMobs')
+
+    for (var i = 0; i <= mobs_array.length - 1; i++) {
+        if (temp[i].id == mobs_array[i]) {
+            first_mobs_list += '<div class="col-4 mt-3 text-center"><h4>' + temp[i].name +
+                '</h4><div class="col-12"><img class="size_img model_img_shadow" src="' + temp[i].image_src +
+                '"></div><div class="col-12"><button type="button" class="btn btn-lg btn-light mt-3 btn_shadow">Fight</button></div></div>'
+        }
+    }
+    HTMLModalWindowForFirstDungeon.insertAdjacentHTML('afterbegin', first_mobs_list)
+}
+
+function push_data(var_name, _var) {
+    localStorage.setItem(var_name, _var)
+}
+
+function pull_data(var_name) {
+    return JSON.parse(localStorage.getItem(var_name))
+}
+
 showDungeonList()
+showFirstMobsList()
